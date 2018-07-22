45650a7a7430924d0e08f6db2fceff3caa396a10
Adosin <Adosin.Lo@gmail.com>
2018-07-22 16:20:10 +0800

    add header

--- products.py
+++ products.py
@@ -18,4 +18,17 @@ for p in products:
 for p in products:
 	print(p[0], '的價格是', p[1]) 
 
+# 字串的加法 'abc' + '123' = 'abc123' 等於合併
+# 字串的乘法 'abc' * 3 = 'abcabcabc' 
+# price 轉進來是字串，由於要寫入，不可以轉為整數(數值)
+# 寫入只能為 '字串'
+# 程式說明 轉整數 price = int(price)
+# 程式說明 轉字串 price = str(price)
+
+print('-----------------') 
+with open('products.csv', 'w', encoding='utf-8') as f: # 當作 f 檔案
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n') 
+
  
